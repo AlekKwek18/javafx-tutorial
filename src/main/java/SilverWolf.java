02d06992e4634753b098517e4701dc20e3c5530c
Alek Kwek <40349610+AlekKwek18@users.noreply.github.com>
2024-09-05 17:07:47 +0800

    Ready

---
+++ src/main/java/SilverWolf.java
@@ -0,0 +1,13 @@
+public class SilverWolf {
+    public static void main(String[] args) {
+        System.out.println("Hello! I am Duke");
+    }
+
+    /**
+     * Generates a response for the user's chat message.
+     */
+    public String getResponse(String input) {
+        return "Silverwolf heard: " + input;
+    }
+}
+
